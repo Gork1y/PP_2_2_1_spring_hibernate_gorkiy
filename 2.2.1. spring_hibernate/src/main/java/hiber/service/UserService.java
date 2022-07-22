450948a5f878b7544efb63ac39e10540cf5b6a08
Bl3cter <blecster@gmail.com>
2022-07-22 17:05:55 +0300

    ex2_2_1

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -3,9 +3,8 @@ package hiber.service;
 import hiber.model.Car;
 import hiber.model.User;
 
-import javax.transaction.Transactional;
 import java.util.List;
-import java.sql.SQLException;
+
 
 public interface UserService {
     void addUser(User user);
